ae2de5c863e42e8ed62850046adac883398b6fa9
KevinIbarra1 <kevin.ibarra@upr.edu>
2024-07-24 21:05:15 -0400

    index image fix for mobile take 3

--- src/components/cta/IndexSectionCta5.js
+++ src/components/cta/IndexSectionCta5.js
@@ -3,24 +3,33 @@ import React from 'react';
 export default function IndexSectionCta5() {
   return (
     <React.Fragment>
-      <section className="relative bg-no-repeat bg-center bg-cover overflow-hidden" style={{ backgroundImage: 'url("image00022.webp")', backgroundAttachment: 'fixed', backgroundPosition: 'center', backgroundSize: 'cover' }}>
-        <div className="absolute inset-0 bg-black bg-opacity-60" style={{ backdropFilter: 'blur(9px)' }}></div>
-        <div className="relative py-40 container px-4 mx-auto text-center text-white">
-          <div className="max-w-3xl mx-auto">
-            <h2 className="mb-4 text-3xl md:text-6xl font-bold">
-              TE <span className="text-blue-NV">AYUDAMOS</span> A <span className="text-pink-NV">CONSTRUIR</span>
-            </h2>
-            <h2 className="mb-4 text-3xl md:text-6xl font-bold">
-              UNA <span className="text-blue-NV">NUEVA</span> <span className="text-pink-NV">VIDA</span>
-            </h2>
-            {/* <div className="inline-block">
-              <button className="py-4 px-6 w-full md:w-auto text-white font-semibold border border-transparent rounded-xl shadow-4xl focus:ring focus:ring-blue-300 bg-blue-500 hover:bg-blue-600 transition ease-in-out duration-200" type="button">
-                Become a member
-              </button>
-            </div> */}
+      <>
+        <section
+          className="bg-cover bg-center bg-no-repeat"
+          style={{ backgroundImage: 'url("image00022.webp")' }}
+        >
+          <div
+            className="bg-black bg-opacity-60 py-40"
+            style={{ backdropFilter: "blur(9px)" }}
+          >
+            <div className="container px-4 mx-auto">
+              <div className="text-center max-w-3xl mx-auto">
+                <h2 className="mb-4 text-white text-3xl md:text-6xl font-bold">
+                  TE <span className="text-blue-NV">AYUDAMOS</span> A <span className="text-pink-NV">CONSTRUIR</span>
+                </h2>
+                <h2 className="mb-4 text-white text-3xl md:text-6xl font-bold">
+                  UNA <span className="text-blue-NV">NUEVA</span> <span className="text-pink-NV">VIDA</span>
+                </h2>
+                {/* <div className="md:inline-block">
+                  <button className="py-4 px-6 w-full md:w-auto text-white font-semibold border border-transparent rounded-xl shadow-4xl focus:ring focus:ring-blue-300 bg-blue-500 hover:bg-blue-600 transition ease-in-out duration-200" type="button">
+                    Become a member
+                  </button>
+                </div> */}
+              </div>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </>
     </React.Fragment>
   );
 }
