64f12118a981fa5bfdff4d2f6d1848f6cd8d9589
KevinIbarra1 <kevin.ibarra@upr.edu>
2024-07-24 20:50:14 -0400

    fix for mobile devices

--- src/components/cta/IndexSectionCta5.js
+++ src/components/cta/IndexSectionCta5.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function IndexSectionCta5() {
   return (
     <React.Fragment>
-      <section className="relative bg-no-repeat bg-center bg-cover bg-fixed overflow-hidden" style={{ backgroundImage: 'url("image00022.webp")' }}>
+      <section className="relative bg-no-repeat bg-center bg-cover overflow-hidden min-h-screen" style={{ backgroundImage: 'url("image00022.webp")' }}>
         <div className="absolute inset-0 bg-black bg-opacity-60" style={{ backdropFilter: 'blur(9px)' }}></div>
         <div className="relative py-40 container px-4 mx-auto text-center text-white">
           <div className="max-w-3xl mx-auto">
@@ -11,7 +11,7 @@ export default function IndexSectionCta5() {
               TE <span className="text-blue-NV">AYUDAMOS</span> A <span className="text-pink-NV">CONSTRUIR</span>
             </h2>
             <h2 className="mb-4 text-3xl md:text-6xl font-bold">
-              UNA  <span className="text-blue-NV">NUEVA</span> <span className="text-pink-NV">VIDA</span>
+              UNA <span className="text-blue-NV">NUEVA</span> <span className="text-pink-NV">VIDA</span>
             </h2>
             {/* <div className="inline-block">
               <button className="py-4 px-6 w-full md:w-auto text-white font-semibold border border-transparent rounded-xl shadow-4xl focus:ring focus:ring-blue-300 bg-blue-500 hover:bg-blue-600 transition ease-in-out duration-200" type="button">
